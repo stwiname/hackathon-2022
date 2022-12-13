4045ab1552d437e5d0bad5ccd1a0e28ce0992e9f
tane-git <tane.alexander.wilson@gmail.com>
2022-12-14 07:57:58 +1300

    add erc20 RentToken, and refactor .env file and its use across proejct

--- packages/contracts/scripts/mintNFT.ts
+++ packages/contracts/scripts/mintNFT.ts
@@ -3,24 +3,21 @@ import { ethers } from "hardhat"
 import "dotenv/config"
 import { MyNFT, MyNFT__factory } from "../dist"
 
-const nftAdress = "0xa2A8C5eC1E1A51E9c79a143E89AB248c18767aA1"
+const NFT_ADDRESS = process.env.NFT_ADDRESS
+const OWNER_ADRESS = process.env.MAIN_ADDRESS
 
 const metaData =
     "https://gateway.pinata.cloud/ipfs/QmTfLu91SDcwmW1zP3meF7mEcPkieYBCMA5HmRnfC2AbbL/ian.json"
 
-const ownerAdress = process.env.ADDRESS
-
 const mint = async () => {
-    console.log("ownerAdress: ", ownerAdress)
-
     const contract = await MyNFT__factory.connect(
-        nftAdress,
-        await ethers.getSigner(ownerAdress!)
+        NFT_ADDRESS!,
+        await ethers.getSigner(OWNER_ADRESS!)
     )
 
     // const contract = await ethers.getContractAt(MyNFT, nftAdress)
 
-    const tx = await contract.mintNFT(ownerAdress!, metaData)
+    const tx = await contract.mintNFT(OWNER_ADRESS!, metaData)
     await tx.wait()
 
     console.log("tx: ", tx)
