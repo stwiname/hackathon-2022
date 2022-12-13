454315a2700b1b0b6fae47f03128bbca5b79e207
tane-git <tane.alexander.wilson@gmail.com>
2022-12-14 12:23:01 +1300

    commment out console.logs (leaving them there though because may need for debugging)

--- packages/contracts/test/Renting.test.ts
+++ packages/contracts/test/Renting.test.ts
@@ -5,24 +5,24 @@ import { epochDuration, epochStart } from "../utils/time"
 import { metaDataEndpoint, people } from "../utils/variables"
 
 const deployNFT = async () => {
-    console.log("deploying NFT...")
+    // console.log("deploying NFT...")
 
     const NFT = await ethers.getContractFactory("MyNFT")
     const nft = await NFT.deploy()
     await nft.deployed()
 
-    console.log("NFT deployed to:", nft.address)
+    // console.log("NFT deployed to:", nft.address)
     return nft
 }
 
 const mintNFTs = async (nft: MyNFT, signers: SignerWithAddress[]) => {
-    console.log("minting NFTs...")
+    // console.log("minting NFTs...")
 
     const mint = async (recipent: string, name: string) => {
-        console.log(`Minting NFT for ${name} at address: ${recipent}`)
+        // console.log(`Minting NFT for ${name} at address: ${recipent}`)
 
         const metaData = `${metaDataEndpoint}/${name}.json`
-        console.log("metaData: ", metaData)
+        // console.log("metaData: ", metaData)
 
         const tx = await nft.connect(signers[0]).mintNFT(recipent, metaData)
         await tx.wait(1)
@@ -32,11 +32,11 @@ const mintNFTs = async (nft: MyNFT, signers: SignerWithAddress[]) => {
         await mint(signers[p + 1].address, people[p].name)
     }
 
-    console.log("NFTs minted")
+    // console.log("NFTs minted")
 }
 
 const deployToken = async () => {
-    console.log("deploying token...")
+    // console.log("deploying token...")
 
     const Token = await ethers.getContractFactory("RentToken")
     const token = await Token.deploy()
@@ -47,7 +47,7 @@ const deployToken = async () => {
 }
 
 const deployRenting = async (nft: string, token: string) => {
-    console.log("deploying renting contract...")
+    // console.log("deploying renting contract...")
 
     const Renting = await ethers.getContractFactory("Renting")
     const renting = await Renting.deploy(nft, token, epochStart, epochDuration)
@@ -59,11 +59,9 @@ const deployRenting = async (nft: string, token: string) => {
 
 describe("Renting", () => {
     const deployRentingFixture = async () => {
-        console.log("deployRentingFixture:")
+        // console.log("deployRentingFixture:")
 
         const signers = await ethers.getSigners()
-        signers.forEach((s) => console.log(s.address))
-
         const nft = await deployNFT()
         const token = await deployToken()
         const renting = await deployRenting(nft.address, token.address)
