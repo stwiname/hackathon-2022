9b09d6f4664965df0c8d074a5326bce663c6eac9
Philip Tian <philip.tian@onfinality.io>
2022-12-14 10:00:18 +1300

    save

--- packages/front-end/src/main.tsx
+++ packages/front-end/src/main.tsx
@@ -4,9 +4,11 @@ import App from "./App"
 import { Routes, Route, BrowserRouter } from "react-router-dom"
 import "./index.css"
 import { HomePage } from "./pages"
+import { Header } from "./component"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
+        <Header/>
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<HomePage />} />
--- packages/front-end/src/pages/HomePage/index.tsx
+++ packages/front-end/src/pages/HomePage/index.tsx
@@ -1,16 +1,43 @@
 import React from "react"
 import { RouteConfigComponentProps } from "react-router-config"
-import { Header } from "../../component"
-
-
+import { Carousel, Row, Col } from "antd"
+import ian from './imgs/ian.jpg'
+import weiqi from './imgs/Weiqi.jpg'
+import robert from './imgs/Rob.jpg'
+import styles from './style.module.scss'
 interface IProps extends RouteConfigComponentProps {}
-
-
 
 export const HomePage: React.FC<IProps> = (Iprops) => {
     return (
-        <>
-            <Header/>
-        </>
+        <div className={styles.container}>
+            <Carousel>
+                <div>
+                    <div className={styles.imgBox}>
+                        <img src={ian} height={320} alt="Ian" />
+                    </div>
+                </div>
+                <div>
+                    <div className={styles.imgBox}>
+                        <img src={weiqi} height={320} alt="Weiqi" />
+                    </div>
+                </div>
+                <div>
+                    <div className={styles.imgBox}>
+                        <img src={robert} height={320} alt="Rob" />
+                    </div>
+                </div>
+            </Carousel>
+            <div className={styles.trendingAndListing}>
+                <Row>
+                    <Col span={5} className={styles.trendingCol}>
+                        <div className={styles.trending}>
+                            <h3>Trending</h3>
+                        </div>
+                    </Col>
+                    <Col span={19}>
+                    </Col>
+                </Row>
+            </div>
+        </div>
     )
 }
