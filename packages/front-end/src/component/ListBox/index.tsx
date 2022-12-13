b47e4d2a15d6b302fb861ca98b3dde0ce3733fb0
Philip Tian <philip.tian@onfinality.io>
2022-12-14 12:29:48 +1300

    “ready to get data”

---
+++ packages/front-end/src/component/ListBox/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { RouteConfigComponentProps } from "react-router-config"
+import { observer } from "mobx-react"
+import styles from './style.module.scss'
+import { Card } from 'antd'
+import { NFTMetadata } from '../../types/contracts/nft'
+interface IProps extends RouteConfigComponentProps {
+  nft: NFTMetadata
+}
+
+export const ListBox: React.FC<IProps> = observer(({nft}) => {
+  return (
+    <div className={styles.container}>
+      <Card title={nft.name}>
+        <p><img src={nft.image} alt={nft.description} height={150}/></p>
+        <p>{nft.description}</p>
+      </Card>
+    </div>
+  )
+})
--- packages/front-end/src/pages/HomePage/index.tsx
+++ packages/front-end/src/pages/HomePage/index.tsx
@@ -6,29 +6,28 @@ import weiqi from "./imgs/Weiqi.jpg"
 import robert from "./imgs/Rob.jpg"
 import styles from "./style.module.scss"
 import { Link } from "react-router-dom"
+import { NFTMetadata } from "../../types/contracts/nft"
+import { ListBox } from "../../component"
+
 interface IProps extends RouteConfigComponentProps {}
-interface nft {
-    name: string
-    image: string
-    description: string
-}
 
 export const HomePage: React.FC<IProps> = (Iprops) => {
-    const nftList: nft[] = [
+    const nftList: NFTMetadata[] = [
         {
-            name: "Ian the brave",
-            image: "/images/ian.jpg",
-            description: "",
+            name: "Ian the Ingenious",
+            image: "./images/ian.jpg",
+            description:
+                "No matter how difficult your coding question is, Ian the Ingenious will be able to answer them all. Not only was he the co-founder of StackOverflow, he actually answered 90% of all the questions under different pseudonym to remain hidden (much like Satoshi), but we have tracked him and and made him available to everyone... for a price of course.",
         },
         {
             name: "Rob the mighty",
-            image: "/images/rob.jpg",
-            description: "",
+            image: "./images/rob.jpg",
+            description: "Super Hero Business Marketing",
         },
         {
             name: "Weiqi the powerful",
-            image: "/images/weiqi.jpg",
-            description: "",
+            image: "./images/weiqi.jpg",
+            description: "Super Hero Social Marketing",
         },
     ]
     return (
@@ -57,12 +56,25 @@ export const HomePage: React.FC<IProps> = (Iprops) => {
                             <h2>Trending</h2>
                             <div>
                                 {nftList.map((e) => {
-                                    return <div className={styles.alink} key={e.name}><Link to={`/${e.name}`}>{e.name}</Link></div>
+                                    return (
+                                        <div
+                                            className={styles.alink}
+                                            key={e.name}
+                                        >
+                                            <Link to={`/${e.name}`}>
+                                                {e.name}
+                                            </Link>
+                                        </div>
+                                    )
                                 })}
                             </div>
                         </div>
                     </Col>
-                    <Col span={19}></Col>
+                    <Col span={19} className={styles.nftCol}>
+                        {nftList.map((e) => {
+                            return <ListBox nft={e} key={e.name}/>
+                        })}
+                    </Col>
                 </Row>
             </div>
         </div>
