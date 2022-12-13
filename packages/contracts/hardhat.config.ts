c5a907852d1e2705623439dbdfb8e4a2767d1305
tane-git <tane.alexander.wilson@gmail.com>
2022-12-13 17:58:27 +1300

    add nft start, add dotenv and add example on how to use backend types in frontend

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -1,6 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "dotenv"
+import "dotenv/config"
 
 const RPC_URL_MOONBASE_APLHA = process.env.RPC_URL_MOONBASE_APLHA
 const PRIVATE_KEY = process.env.PRIVATE_KEY
