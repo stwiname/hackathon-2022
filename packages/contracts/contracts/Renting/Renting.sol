6ea76c3c00e5b3eadd1cf987e8584731f3b1cf08
tane-git <tane.alexander.wilson@gmail.com>
2022-12-14 10:14:28 +1300

    clean up

--- packages/contracts/contracts/Renting/Renting.sol
+++ packages/contracts/contracts/Renting/Renting.sol
@@ -8,47 +8,8 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
-
 import "./IRenting.sol";
 
-// interface IERC1155Rentable is IERC1155 {
-//   /* The address of the base ERC1155 asset */
-//   function baseAssetAddress() external view returns (address);
-
-//   // Duration of a rental period
-//   function epochDuration() external view returns (uint);
-
-//   // Starting point of the first epoch
-//   function epochStart() external view returns (uint);
-
-//   struct Rental {
-//     uint256 price;
-//     uint256 startEpoch;
-//     uint256 numEpochs;
-//   }
-
-//   /* Sets the rental `price` per period for a given `id` */
-//   function setPrice(uint256 tokenId, uint256 price) external;
-
-//   /* Get the rental price for a given id */
-//   function priceOf(uint256 tokenId) external view returns (uint256);
-
-//   /* Rent an asset with given `id` for a number of `periods` */
-//   function rent(uint256 tokenId, uint periods) external;
-
-//   /* End all uncomplete rentals early for a given `id`, returning the rent for unused periods */
-//   function release(uint256 tokenId) external;
-
-//   /* Collect any uncollected rent for an epoch */
-//   function collect(uint256 tokenId, uint256 epochId) external;
-
-//   /* Collect any outstanding rent up to current epoch */
-//   function batchCollect(uint256 tokenId) external;
-
-//   function rentalsOf(address account, uint256 tokenId) external view returns (Rental[] memory);
-// }
-
-// contract Renting is ERC165, IERC1155, IERC1155Rentable {
 contract Renting is ERC165, IERC1155, IRenting {
 
   using Address for address;
