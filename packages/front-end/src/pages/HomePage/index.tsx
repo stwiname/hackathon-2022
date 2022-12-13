614183eef3afba535cb4c29caa3bf40fcc290b36
Philip Tian <philip.tian@onfinality.io>
2022-12-14 11:33:23 +1300

    save

--- packages/front-end/src/pages/HomePage/index.tsx
+++ packages/front-end/src/pages/HomePage/index.tsx
@@ -1,13 +1,36 @@
 import React from "react"
 import { RouteConfigComponentProps } from "react-router-config"
 import { Carousel, Row, Col } from "antd"
-import ian from './imgs/ian.jpg'
-import weiqi from './imgs/Weiqi.jpg'
-import robert from './imgs/Rob.jpg'
-import styles from './style.module.scss'
+import ian from "./imgs/ian.jpg"
+import weiqi from "./imgs/Weiqi.jpg"
+import robert from "./imgs/Rob.jpg"
+import styles from "./style.module.scss"
+import { Link } from "react-router-dom"
 interface IProps extends RouteConfigComponentProps {}
+interface nft {
+    name: string
+    image: string
+    description: string
+}
 
 export const HomePage: React.FC<IProps> = (Iprops) => {
+    const nftList: nft[] = [
+        {
+            name: "Ian the brave",
+            image: "/images/ian.jpg",
+            description: "",
+        },
+        {
+            name: "Rob the mighty",
+            image: "/images/rob.jpg",
+            description: "",
+        },
+        {
+            name: "Weiqi the powerful",
+            image: "/images/weiqi.jpg",
+            description: "",
+        },
+    ]
     return (
         <div className={styles.container}>
             <Carousel>
@@ -31,11 +54,15 @@ export const HomePage: React.FC<IProps> = (Iprops) => {
                 <Row>
                     <Col span={5} className={styles.trendingCol}>
                         <div className={styles.trending}>
-                            <h3>Trending</h3>
+                            <h2>Trending</h2>
+                            <div>
+                                {nftList.map((e) => {
+                                    return <div className={styles.alink} key={e.name}><Link to={`/${e.name}`}>{e.name}</Link></div>
+                                })}
+                            </div>
                         </div>
                     </Col>
-                    <Col span={19}>
-                    </Col>
+                    <Col span={19}></Col>
                 </Row>
             </div>
         </div>
